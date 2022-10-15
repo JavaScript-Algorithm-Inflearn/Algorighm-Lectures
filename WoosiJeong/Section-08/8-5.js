5e1d02f294b9535db029d79d69897f6caf39abc6
wusi-hub <jws9595@naver.com>
2022-10-15 20:44:09 +0900

    SOLVED / 8-7

---
+++ WoosiJeong/Section-08/8-5.js
@@ -0,0 +1,23 @@
+function solution(arr){
+    let answer="NO", flag=0;
+    let total=arr.reduce((a, b)=>a+b, 0);
+    let n=arr.length;
+    function DFS(L, sum){
+        if(flag) return;
+        if(L===n){
+            if((total-sum)===sum){
+                answer="YES";
+                flag=1;
+            }
+        }
+        else{
+            DFS(L+1, sum+arr[L]);
+            DFS(L+1, sum);
+        }
+    }
+    DFS(0, 0);
+    return answer;
+}
+
+let arr=[1, 3, 5, 6, 7, 10];
+console.log(solution(arr));
